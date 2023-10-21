414daeed18f11d0c4ed3d013bf661d8c22d493e6
OwenCochell <owencochell@gmail.com>
2023-10-21 02:21:36 -0400

    Initial commit

---
+++ eeprom/eromdump.lua
@@ -0,0 +1,27 @@
+-- Dumps the current ROM to a file
+-- The file can be provided on the command line,
+-- otherwise we simply dump to errprom.dump
+
+-- Define the path
+
+local path = 'errprom.dump'
+
+-- Include the components:
+
+local component = require("component")
+
+-- Get the EERP Contents
+
+local contents = component.eeprom.get()
+
+-- Open file to save to
+
+local file = io.open(path, "w")
+
+-- Write file contents:
+
+file:write(contents)
+
+-- Close file contents
+
+file:close()
