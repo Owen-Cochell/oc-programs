7e2baaf5ff1647672c74c1d482f34e4be89a3002
OwenCochell <owencochell@gmail.com>
2023-10-22 02:25:47 -0400

    Added display program

---
+++ oscreen/oscreen_display.lua
@@ -0,0 +1,56 @@
+-- Displays arbitrary text to a screen.
+-- We simply display the text, and change the viewport
+-- to only display the given text.
+
+-- Parse arguments
+
+local shell = require("shell")
+local args, ops = shell.parse(...)
+
+-- Determine if we have arguments:
+
+local text = ''
+
+if (#args < 1)
+then
+    -- Not enough arguments
+
+    print("Error - Must provide text to display!")
+    os.exit()
+else
+    -- Define the path as the first argument
+
+    text = args[1]
+end
+
+-- Get the terminal
+
+local term = require("term")
+
+-- Get the GPU
+
+local gpu = term.gpu()
+
+-- Reset the screen
+
+term.clear()
+
+-- Write some text
+
+term.write(text)
+
+-- get the current cursor position
+
+local x, y = term.getCursor()
+
+-- Set the viewpoint:
+
+gpu.setViewport(x, y)
+
+-- Move the cursor out of the viewport:
+
+term.setCursor(x+1, y+1)
+
+-- Read forever
+
+io.read()
