bf5595b9f8f1f1910484ac476fd464ce55b6a385
OwenCochell <owencochell@gmail.com>
2023-10-23 00:38:17 -0400

    Added install step

--- security/security_install.lua
+++ security/security_install.lua
@@ -2,4 +2,5 @@
 
 local shell = require("shell")
 
-shell.execute("wget -f https://raw.githubusercontent.com/cadergator10/Opencomputers-servertine/main/database/Boot.lua boot.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/cadergator10/Opencomputers-servertine/main/database/Boot.lua database.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/cadergator10/Opencomputers-servertine/main/server/server.lua server.lua")
